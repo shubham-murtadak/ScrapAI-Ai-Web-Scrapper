7e4accb92f193903ae5ac9fd9b90dcfaaf270faa
shubham murtadak <shubhammurtadak022@gmail.com>
2024-09-26 02:21:17 +0530

    version I done !

---
+++ main.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from Source.scrap import (scrape_website,extract_body_content,clean_body_content,split_dom_content)
+from Source.ollama import parse_with_ollama
+# Streamlit UI
+st.title("Ai Web Scrapper !")
+
+url=st.text_input("Enter a website url :")
+
+
+# Step 1: Scrape the Website
+if st.button('Scrap site'):
+    st.write("Scrapping the website...")
+
+     # Scrape the website
+    dom_content = scrape_website(url)
+    body_content = extract_body_content(dom_content)
+    cleaned_content = clean_body_content(body_content)
+
+    # Store the DOM content in Streamlit session state
+    st.session_state.dom_content = cleaned_content
+
+     # Display the DOM content in an expandable text box
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_content, height=300)
+
+
+
+# Step 2: Ask Questions About the DOM Content
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content...")
+
+            # Parse the content with Ollama
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            parsed_result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(parsed_result)
